9f01a82f9471f92173e69f93c666f7f371339df3
makarov.vladimir@rt.ru <makarov.vladimir@rt.ru>
2023-05-22 11:27:27 +0300

    1

---
+++ src/main/java/com/example/testgradle/test/sealedclasses/gitreverttest/TestRevert.java
@@ -0,0 +1,12 @@
+package com.example.testgradle.test.sealedclasses.gitreverttest;
+
+public class TestRevert {
+
+    private Integer number;
+
+    public static Integer testNumber() {
+        int i = 5;
+
+        return i;
+    }
+}
